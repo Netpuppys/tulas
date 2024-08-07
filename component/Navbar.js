0a8f8a0131634ef726dc2f9310469d3f6732e6d8
Harsh Rattewal <harshrattewal@gmail.com>
2024-08-07 16:14:07 +0530

    Navbar arrow

--- component/Navbar.js
+++ component/Navbar.js
@@ -2,7 +2,6 @@
 import React, { useState } from "react";
 import Link from "next/link";
 import TulasLogo from "../public/Components/Navbar/TulasLogo.png";
-import { LuCornerDownRight } from "react-icons/lu";
 import { IoIosArrowForward } from "react-icons/io";
 import { FaPen } from "react-icons/fa";
 import sitemap from "../data/sitemap";
@@ -120,8 +119,21 @@ function Navbar() {
         </div>
         {isChecked && (
           <div className="flex w-full pr-[160px] flex-row z-10">
-            <LuCornerDownRight className="-mt-[15px] ml-[55px] text-white font-thin text-[60px]" />
-            <div className="navbar-list-main text-[20px] flex flex-row w-full justify-between z-10">
+            <div className="-mt-[12px] ml-[60px] text-white font-thin text-[60px]">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="27"
+                height="72"
+                viewBox="0 0 27 72"
+                fill="none"
+              >
+                <path
+                  d="M1.49973 55.5L1.99433 55.5733L1.99973 55.5368V55.5H1.49973ZM26.5 68.5L21.6392 65.3846L21.3715 71.1518L26.5 68.5ZM0.999732 0V55.5H1.99973V0H0.999732ZM1.00513 55.4267C0.66314 57.7352 0.732701 61.1951 2.09861 64.0531C2.78569 65.4907 3.80684 66.7875 5.27765 67.6957C6.74997 68.6049 8.63796 69.1032 11.0215 68.9995L10.978 68.0005C8.76151 68.0968 7.07866 67.6326 5.80306 66.8449C4.52596 66.0563 3.6221 64.9218 3.00086 63.6219C1.7501 61.0049 1.66966 57.7648 1.99433 55.5733L1.00513 55.4267ZM11.0215 68.9995C15.7988 68.7918 19.4293 68.7553 21.9976 68.7913L22.0116 67.7914C19.4215 67.7551 15.7717 67.7921 10.978 68.0005L11.0215 68.9995Z"
+                  fill="white"
+                />
+              </svg>
+            </div>
+            <div className="navbar-list-main mt-[40px] pl-10 text-[20px] flex flex-row w-full justify-between z-10">
               {sitemap.map((item, index) => (
                 <div
                   className="main-list-item"
