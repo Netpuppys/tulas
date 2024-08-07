e8444c0edf9dd7fcc058ae0df7d397c81a3e3fc9
Harsh Rattewal <harshrattewal@gmail.com>
2024-08-07 16:02:00 +0530

    Why Tulas

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@ import React from "react";
 import Banner from "@/component/Banner";
 import Navbar from "@/component/Navbar";
 import AboutTulas from "./Component/AboutTulas";
+import WhyTulasScroll from "./Component/WhyTulasScroll";
 
 export const metadata = {
   title:
@@ -15,6 +16,7 @@ export default function Home() {
       <Navbar />
       <Banner />
       <AboutTulas />
+      <WhyTulasScroll />
     </>
   );
 }
