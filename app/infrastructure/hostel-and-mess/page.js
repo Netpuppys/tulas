d7c45940a2a0798bb8eebb798120bb6d54663870
Harsh Kumar rattewal <harshrattewal@gmail.com>
2024-09-05 15:01:16 +0530

    2 more pages

---
+++ app/infrastructure/hostel-and-mess/page.js
@@ -0,0 +1,193 @@
+"use client";
+
+import Banner from "@/component/Banner";
+import BannerImg from "../../../public/infrastructure/library/bannerImg.png";
+import Navbar from "@/component/Navbar";
+import Image from "next/image";
+import Footer from "@/component/Footer";
+import background from "../../../public/infrastructure/HostelMess/background.png";
+import { Fade } from "react-awesome-reveal";
+import biggestAuditorium from "../../../public/infrastructure/auditorium/biggestAuditorium.png";
+import { useInView } from "react-intersection-observer";
+import hostel from "../../../public/infrastructure/HostelMess/hostel.png";
+import vibgyor from "../../../public/infrastructure/HostelMess/vibgyor.png";
+import multiUse from "../../../public/infrastructure/auditorium/multiUse.png";
+
+function LectureTheater() {
+  const { ref, inView } = useInView({
+    triggerOnce: false, // This allows the animation to trigger every time the component comes into view
+    threshold: 0.1, // 10% of the element must be visible to trigger
+  });
+  const sections = [
+    {
+      title: "Mess ( Only Vegetarian )",
+      description:
+        "The students in the hostels roll only have the right to enter into mess premises. The students are required to come to the mess during their specified timing only, not before or after. The students govern mess Committee; the mess committee makes sure that students’ health and nutrition is well taken care of. Mess serves breakfast, lunch, evening tea and dinner every day and the weekly menu (Chinese, North and South Indian cuisines) is available with mess in-charge; the students make menu, and the cuisine is changed twice a month.",
+      image: biggestAuditorium,
+    },
+    {
+      title: "Cafeteria",
+      description: (
+        <>
+          A favorite spot for any student in any college is always the
+          Cafeteria. It is the place where one can drop by in any frame of mind
+          and still have a great time. It is here that success is enjoyed and
+          victory is celebrated; friends interact the most, solace given and
+          basically a place to chill out. College cafeteria not only serve basic
+          cuisines but also serves students favorite pasta, macaroni, dim sums,
+          pizza, shakes etc at reasonable price. Snacks, soft drinks and fresh
+          juices are also available. It functions during Morning 8:30 to Evening
+          10:15 which extends to 2:30AM during examinations.
+          <br />
+          <br />
+          Maggie Point (next to the junior hostel) is open for 11:00PM to 2:30AM
+          daily
+        </>
+      ),
+      image: multiUse,
+    },
+  ];
+  return (
+    <div className="flex flex-col items-center">
+      <Navbar />
+      <Banner
+        image={BannerImg}
+        title={
+          <>
+            HOSTEL & MESS <span className="text-[#007A83]">FACILITIES</span>
+          </>
+        }
+        description={<></>}
+        belowTitle={<></>}
+        belowPara={
+          "Tula’s Institute is one of the best engineering colleges in Dehradun, which provides the best hostel life; the hostel atmosphere forms an essential part of the young budding engineers and management graduates. 6 hostels are located in Tula’s campus, out of which 4 are boy’s hostels and 2 are girl’s hostels. Each room is on twin sharing; rooms are spacious, ventilated, airy, and well lit."
+        }
+      />
+      {/* fixed background */}
+      <div className="w-full relative overflow-hidden -mb-[6vh]">
+        <Image
+          src={background}
+          className="w-full fixed h-screen top-0 left-0 -z-10 object-cover"
+          alt=""
+        />
+      </div>
+      <div className="pt-40 pb-20 bg-transparent w-[95%] ml-[5%] h-fit flex items-center justify-between">
+        <Fade delay={300} className="w-[45%]">
+          <div className="w-full block">
+            <h3 className="px-10 font-[TTChocolatesBold] font-semibold rounded-md text-[52px] border-l-4 border-white">
+              Facilities for a Hosteler
+            </h3>
+            <br />
+            <h4 className="text-[20px] font-[TTChocolates] text-[#D9D9D9]">
+              <ul className="list-disc ml-4">
+                <li>24-hour WIFI facility</li>
+                <li>Separate table and chair for every hosteler</li>
+                <li>Separate Book Shelves</li>
+                <li>Attached Balcony for each room</li>
+                <li>Solar heated water 24×7</li>
+                <li>Single & Double occupancy rooms are available</li>
+              </ul>
+              <br />
+              Hostel life is an excellent opportunity for the development and
+              enrichment of the character and personality of inmates. Tula’s
+              Institute is known for being the best engineering college in
+              Dehradun for providing 24×7 security for the students living in
+              the hostel and making a living on campus safe and comfortable.
+              <br />
+              <br />
+              A fully qualified MBBS Doctor is available at the campus.
+              <br />
+              <br />
+              Add-on facilities for students living on campus:
+              <br />
+              <ul className="list-disc ml-4">
+                <li>Foreign Language learning</li>
+                <li>Personality Development classes</li>
+                <li>Cricket Ground</li>
+                <li>Volley Ball Court</li>
+                <li>Basket Ball Court</li>
+                <li>Table Tennis Football / Hockey Ground</li>
+                <li>Recreation Room for Indoor Games</li>
+                <li>Modern Gymnasium</li>
+                <li>Television Room</li>
+                <li>Yoga/ Meditation Room</li>
+              </ul>
+            </h4>
+          </div>
+        </Fade>
+        <Fade className="w-[45%]">
+          <Image src={hostel} alt="" className="w-full h-fit" />
+        </Fade>
+      </div>
+      <div
+        ref={ref}
+        className="w-full h-fit py-20 bg-white px-[15%] flex justify-between items-center"
+      >
+        <div className="w-[40%] h-fit flex justify-center items-center">
+          <div className="flex flex-col justify-center items-center">
+            <h3
+              className={`font-[CarotSlab] font-medium text-[#3D001B] text-[40px] transition-transform duration-1000 ${
+                inView ? "animate-translateLeft" : "opacity-0"
+              }`}
+            >
+              Student Council-Vibgyor
+            </h3>
+            <Image
+              src={vibgyor}
+              alt=""
+              className={`w-fit h-fit ${
+                inView ? "animate-translateLeft" : "opacity-0"
+              }`}
+            />
+          </div>
+        </div>
+        <h4
+          className={`text-black w-[50%] max-w-[669px] text-[22px] font-[TTChocolates] font-medium ${
+            inView ? "animate-translateRight" : "opacity-0"
+          }`}
+        >
+          This Committee is a student-run and student-governed Committee
+          committed to provide unique, exciting, diverse, fun, educational
+          atmosphere for the hostelers. Committee organizes regular movies shows
+          at the auditorium, inter hostel sports matches, recreational trips.
+          This Committee also focuses on keeping the secular harmony among the
+          students by celebrating major religious festivals.
+        </h4>
+      </div>
+      <div className="pt-40 w-[90%] mx-auto bg-transparent h-fit">
+        {sections.map((section, index) => (
+          <div
+            key={index}
+            className={`flex mb-20 justify-between w-full h-fit items-center ${
+              index % 2 !== 0 ? "flex-row-reverse" : ""
+            } `}
+          >
+            <Fade delay={300} className="w-[45%]">
+              <div className="w-full block">
+                <h3 className="px-10 font-[TTChocolatesBold] font-semibold rounded-md text-[52px] border-l-4 border-white">
+                  {section.title}
+                </h3>
+                <br />
+                <h4 className="text-[22px] font-[TTChocolates] text-[#D9D9D9]">
+                  {section.description}
+                </h4>
+              </div>
+            </Fade>
+            <Fade className="w-[45%]">
+              <Image
+                src={section.image}
+                alt=""
+                className="w-full h-fit rounded-3xl"
+              />
+            </Fade>
+          </div>
+        ))}
+      </div>
+      <div className="w-full h-fit z-50">
+        <Footer color={true} />
+      </div>
+    </div>
+  );
+}
+
+export default LectureTheater;
---
+++ app/infrastructure/other-facilities/page.js
@@ -0,0 +1,204 @@
+"use client";
+
+import Banner from "@/component/Banner";
+import BannerImg from "../../../public/infrastructure/library/bannerImg.png";
+import Navbar from "@/component/Navbar";
+import Image from "next/image";
+import Footer from "@/component/Footer";
+import background from "../../../public/infrastructure/HostelMess/background.png";
+import { Fade } from "react-awesome-reveal";
+import { useInView } from "react-intersection-observer";
+import wifi from "../../../public/infrastructure/Other/wifi.png";
+import atm from "../../../public/infrastructure/Other/atm.png";
+import Gymnasium from "../../../public/infrastructure/Other/gymnasium.png";
+import guestHouse from "../../../public/infrastructure/Other/guestHouse.png";
+import medical from "../../../public/infrastructure/Other/medical.png";
+import playGround from "../../../public/infrastructure/Other/playGround.png";
+
+const sectionsData = [
+  {
+    title: "Wifi Facility",
+    description: (
+      <>
+        Availability of 24Hrs Wi-Fi internet access in the campus with 205 mbps
+        speed and Comprehensive e-learning environment provided in the campus,
+        enables all the students and faculty members to be connected which is
+        essential for effective learning.
+        <br />
+        <br />
+        Our Digital library has over 5800+ e-books available round the clock
+        through a FTP server on the intranet. Students can access the material
+        and books anytime from anywhere on the campus.
+      </>
+    ),
+    image: wifi,
+  },
+  {
+    title: "ATM Facility",
+    description:
+      "24/7 ATM runs within the Oriental Bank of Commerce campus, making transactions as quick as the blink of an eye. Parents or guardians of students can transfer money online to students, which they can receive immediately. Money transactions can never be an obstacle in a student’s life at Tula’s.",
+    image: atm,
+  },
+  {
+    title: "Guest House",
+    description:
+      "The institute provides guest room facilities within the campus for students’ parents, relatives and guests coming to meet their wards. They can avail the facility for a brief stay along with meals from the hostel mess, and cafeteria. However they are charged a nominal amount for the same.",
+    image: guestHouse,
+  },
+  {
+    title: "Gymnasium",
+    description:
+      "The students have access to a well-equipped gymnasium, allowing them to work out at a time convenient to them. The gym is open in the morning and in the evening. Various equipment like Treadmill, Rowing machine, Cycle, Cross trainer, Twister, Steppers, Ab rollers etc. have been provided. Special trainers have also been employed for the gym. This has been done keeping in mind that a healthy mind can only exist in a healthy body.",
+    image: Gymnasium,
+  },
+  {
+    title: "Medical",
+    description: (
+      <>
+        A shopping arcade has been set up within the campus. It has the facility
+        of laundry, a barber’s shop, stationary and a tuck shop. Reprographic
+        facility is also available.
+        <br />
+        <ul className="list-disc ml-4">
+          <li>
+            First aid boxes have been kept in the Labs, workshop, hostel blocks
+            and with the receptionist.
+          </li>
+          <li>
+            An Ambulance with a driver has been kept stand-by so as to cater to
+            the emergent demand of students.
+          </li>
+          <li>
+            The in-house medical facility comprises of basic amenities providing
+            treatment by a specialized medical staff within the campus. A
+            qualified Physician who is our full time medical consultant is also
+            available 24×7 on call. An infirmary in the campus caters to small
+            ailments.
+          </li>
+          <li> Hospitals of repute have been kept on the panel.</li>
+          <li>
+            To keep a check on diseases administration of insecticides is done
+            to ensure clean & hygienic ambience avoiding several health hazards.
+          </li>
+        </ul>
+      </>
+    ),
+    image: medical,
+  },
+  {
+    title: "Play Ground",
+    description:
+      "A play ground facility is available for sports like cricket, football, volley ball, throw ball, basket ball, badminton and many other outdoor games.A play ground facility is available for sports like cricket, football, volley ball, throw ball, basket ball, badminton and many other outdoor games.",
+    image: playGround,
+  },
+];
+
+const Section = ({ section, index }) => (
+  <div
+    className={`flex mb-20 justify-between w-full h-fit items-center ${
+      index % 2 !== 0 ? "flex-row-reverse" : ""
+    } `}
+  >
+    <Fade delay={300} className="w-[45%]">
+      <div className="w-full block">
+        <h3 className="px-10 font-[TTChocolatesBold] font-semibold rounded-md text-[52px] border-l-4 border-white">
+          {section.title}
+        </h3>
+        <br />
+        <h4 className="text-[22px] font-[TTChocolates] text-[#D9D9D9]">
+          {section.description}
+        </h4>
+      </div>
+    </Fade>
+    <Fade className="w-[45%]">
+      <Image
+        src={section.image}
+        alt={section.title}
+        className="w-full h-fit rounded-3xl"
+      />
+    </Fade>
+  </div>
+);
+
+function OtherFacilities() {
+  return (
+    <div className="flex flex-col items-center">
+      <Navbar />
+      <Banner
+        image={BannerImg}
+        title={
+          <>
+            OTHER <span className="text-[#007A83]">FACILITIES</span>
+          </>
+        }
+        description={<></>}
+        belowTitle={<></>}
+        belowPara={
+          "Tula’s Institute is one of the best engineering colleges in Dehradun, which provides the best hostel life; the hostel atmosphere forms an essential part of the young budding engineers and management graduates. 6 hostels are located in Tula’s campus, out of which 4 are boy’s hostels and 2 are girl’s hostels. Each room is on twin sharing; rooms are spacious, ventilated, airy, and well lit."
+        }
+      />
+      {/* fixed background */}
+      <div className="w-full relative overflow-hidden -mb-[6vh]">
+        <Image
+          src={background}
+          className="w-full fixed h-screen top-0 left-0 -z-10 object-cover"
+          alt="Background"
+        />
+      </div>
+
+      <div className="pt-40 w-[90%] mx-auto bg-transparent h-fit">
+        {sectionsData.slice(0, 2).map((section, index) => (
+          <Section key={index} section={section} index={index} />
+        ))}
+      </div>
+
+      <div className="w-full h-fit py-20 bg-white px-[15%] flex flex-col justify-between items-center">
+        <h3 className="font-[CarotSlab] font-medium text-[#3D001B] text-[40px] transition-transform duration-1000">
+          Book Bank
+        </h3>
+        <h4 className="text-black w-[80%] max-w-[928px] text-[22px] font-[TTChocolates] text-center font-medium">
+          Book bank concept has been implemented in the institute. Under this
+          scheme, every student gets 6 to 8 books for the complete semester. In
+          addition to the support available from library, this scheme supports
+          students to get latest set of books of core subjects for each semester
+          from the college without any hassle. Every student is required to pay
+          a nominal annual fee to benefit from this scheme.
+        </h4>
+      </div>
+
+      <div className="pt-40 w-[90%] mx-auto bg-transparent h-fit">
+        {sectionsData.slice(2, 4).map((section, index) => (
+          <Section key={index} section={section} index={index} />
+        ))}
+      </div>
+
+      <div className="w-full h-fit py-20 bg-white px-[15%] flex flex-col justify-between items-center">
+        <h3 className="font-[CarotSlab] font-medium text-[#3D001B] text-[40px] transition-transform duration-1000">
+          Shopping Arcade
+        </h3>
+        <h4 className="text-black w-[80%] max-w-[928px] text-[22px] font-[TTChocolates] text-center font-medium">
+          A shopping arcade has been set up within the campus. It has the
+          facility of laundry, a barber’s shop, stationary and a tuck shop.
+          Reprographic facility is also available. First aid boxes have been
+          kept in the Labs, workshop, hostel blocks and with the receptionist.
+          An Ambulance with a driver has been kept stand-by so as to cater to
+          the emergent demand of students. The in-house medical facility
+          comprises of basic amenities providing treatment by a specialized
+          medical staff within the campus. A qualified Physician who is our full
+          time medical consultant is also available 24×7 on call. An infirmary
+          in the campus caters to small ailments.
+        </h4>
+      </div>
+
+      <div className="pt-40 w-[90%] mx-auto bg-transparent h-fit">
+        {sectionsData.slice(4).map((section, index) => (
+          <Section key={index} section={section} index={index} />
+        ))}
+      </div>
+
+      <Footer />
+    </div>
+  );
+}
+
+export default OtherFacilities;
