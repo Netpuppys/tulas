03c96238ecd1804bc3948ad3f91a04f106b4a98f
Harsh Kumar rattewal <harshrattewal@gmail.com>
2024-09-24 18:29:34 +0530

    BSC agriculture

--- component/BannerProgram/BannerProgram.js
+++ component/BannerProgram/BannerProgram.js
@@ -11,11 +11,11 @@ function BannerProgram({ image, title, titleColor, description }) {
           className="w-full h-full object-cover absolute rounded-3xl"
         />
       </div>
-      <div className="-mt-[10vh] w-fit z-10 relative">
-        <h3 className="text-[40px] md:text-[60px] text-black z-10 font-[CarotSlab]">
+      <div className="-mt-[18vh] md:-mt-[10vh] w-[80%] md:w-[50%] z-10 relative">
+        <h3 className="text-[38px] leading-none md:leading-normal mb-2 md:mb-0 md:text-[60px] text-black z-10 font-[CarotSlab]">
           {title} <span className="text-[#760135]">{titleColor}</span>
         </h3>
-        <h4 className="w-full md:w-[50%] text-sm md:text-lg text-black font-[TTChocolates]">
+        <h4 className="w-full text-sm md:text-lg text-black font-[TTChocolates]">
           {description}
         </h4>
       </div>
