659f9310b2710b4050e87a7c95a5d76821602e86
shourya mishra <shourya.desk@gmail.com>
2024-11-11 15:31:00 +0530

    sports page

--- app/sports/page.js
+++ app/sports/page.js
@@ -33,10 +33,10 @@ const TextBanner = ({ filledText }) => {
                 WebkitTextStrokeWidth: "0.1px",
               }
         }
-        className="font-[TTChocolatesbold] text-center text-[6rem] font-black tracking-[0.10369rem]  leading-[2.38494rem]"
+        className="font-[TTChocolatesbold] text-center text-[3.5rem] md:text-[6rem] font-black tracking-[0.10369rem] leading-[4rem] md:leading-[2.38494rem]"
       >
         EXPLORE MORE
-        <span className="text-[7rem] block leading-[7.64388rem]">Sports</span>
+        <span className="text-[4.5rem] md:text-[7rem] block md:leading-[7.64388rem]">Sports</span>
       </p>
     </div>
   );
@@ -134,55 +134,177 @@ function Sports() {
           <div className="relative min-h-screen h-fit">
             <Image
               src={background}
-              className="absolute left-0 w-full h-fit object-cover"
+              className="fixed top-0 -z-10 left-0 w-full h-full object-cover"
               alt=""
             />
 
-            {/* <div className="fixed top-0 left-0 z-[10]">
-            <TextBanner
-              filledText={true}
-            />
-          </div>
+            <div ref={contactRef} className="w-screen absolute top-[100vh] h-[calc(100%-200vh)] opacity-0"></div>
 
-          <div className={`${isInView? "fixed" : "absolute"} top-0 left-0 z-[50]`}>
-            <TextBanner
-              filledText={false}
-            />
-          </div>
-          
-          {!isInView &&
-          <div className={`${isInView? "fixed" : "absolute"} bottom-0 left-0 z-[10]`}>
-            <TextBanner
-              filledText={true}
-            />
-          </div>}
+            <div className={`${isInView? "fixed" : "absolute"} top-0 left-0 z-[10]`}>
+              <TextBanner
+                filledText={true}
+              />
+            </div>
 
-          {!isInView &&
-          <div className={`${isInView? "fixed" : "absolute"} bottom-0 left-0 z-[50]`}>
-            <TextBanner
-              filledText={false}
-            />
-          </div>}
+            <div className={`${isInView? "fixed" : "absolute"} top-0 left-0 z-[50]`}>
+              <TextBanner
+                filledText={false}
+              />
+            </div>
+            
+            {!isInView &&
+            <div className={`${isInView? "fixed" : "absolute"} bottom-0 left-0 z-[10]`}>
+              <TextBanner
+                filledText={true}
+              />
+            </div>}
 
-          <div className="w-full h-fit flex pt-10 md:pt-0 flex-col md:flex-row flex-wrap md:gap-y-[5rem] z-[30] relative">
+            {!isInView &&
+            <div className={`${isInView? "fixed" : "absolute"} bottom-0 left-0 z-[50]`}>
+              <TextBanner
+                filledText={false}
+              />
+            </div>}
 
-              <div className="w-full md:w-1/2 py-5 px-5 md:px-0 md:py-0 md:h-[50vh] relative flex items-center justify-start md:justify-center">
-                <div 
-                  style={{
-                    transform: `translate(-${movementX}rem, ${movementY}rem)`,
-                    transition: "transform 0.2s",
-                  }}
-                  className="rounded-full bg-transparent w-56 md:w-80 aspect-square "
-                >
-                  <Image
-                    src={bubble1}
-                    className="w-full h-fit"
-                    alt="bubble"
-                  />
+            <div className="w-full h-fit flex pt-10 md:pt-0 flex-col md:flex-row flex-wrap md:gap-y-[5rem] z-[30] relative">
+
+                <div className="w-full md:w-1/2 py-5 px-5 md:px-0 md:py-0 md:h-[50vh] relative flex items-center justify-start md:justify-center">
+                  <div 
+                    style={{
+                      transform: `translate(-${movementX}rem, ${movementY}rem)`,
+                      transition: "transform 0.2s",
+                    }}
+                    className="rounded-full bg-transparent w-56 md:w-80 aspect-square "
+                  >
+                    <Image
+                      src={bubble1}
+                      className="w-full h-fit"
+                      alt="bubble"
+                    />
+                  </div>
                 </div>
-              </div>
-            ))}
-          </div> */}
+
+                <div className="w-full md:w-1/2 py-5 px-5 md:px-0 md:py-0 md:h-[50vh] relative flex items-center justify-end md:justify-center">
+                  <div 
+                    style={{
+                      transform: `translate(${movementX}rem, ${movementY+2}rem)`,
+                      transition: "transform 0.2s",
+                    }}
+                    className="rounded-full bg-transparent w-56 md:w-80 aspect-square "
+                  >
+                    <Image
+                      src={bubble2}
+                      className="w-full h-fit"
+                      alt="bubble"
+                    />
+                  </div>
+                </div>
+
+                <div className="w-full md:w-1/2 py-5 px-5 md:px-0 md:py-0 md:h-[50vh] relative flex items-center justify-start md:justify-center">
+                  <div 
+                    style={{
+                      transform: `translate(-${movementX}rem, ${movementY}rem)`,
+                      transition: "transform 0.2s",
+                    }}
+                    className="rounded-full bg-transparent w-56 md:w-80 aspect-square "
+                  >
+                    <Image
+                      src={bubble3}
+                      className="w-full h-fit"
+                      alt="bubble"
+                    />
+                  </div>
+                </div>
+
+                <div className="w-full md:w-1/2 py-5 px-5 md:px-0 md:py-0 md:h-[50vh] relative flex items-center justify-end md:justify-center">
+                  <div 
+                    style={{
+                      transform: `translate(-${movementX}rem, -${movementY}rem)`,
+                      transition: "transform 0.2s",
+                    }}
+                    className="rounded-full bg-transparent w-56 md:w-80 aspect-square "
+                  >
+                    <Image
+                      src={bubble4}
+                      className="w-full h-fit"
+                      alt="bubble"
+                    />
+                  </div>
+                </div>
+
+                <div className="w-full md:w-1/2 py-5 px-5 md:px-0 md:py-0 md:h-[50vh] relative flex items-center justify-start md:justify-center">
+                  <div 
+                    style={{
+                      transform: `translate(${movementX+1}rem, ${movementY}rem)`,
+                      transition: "transform 0.2s",
+                    }}
+                    className="rounded-full bg-transparent w-56 md:w-80 aspect-square "
+                  >
+                    <Image
+                      src={bubble5}
+                      className="w-full h-fit"
+                      alt="bubble"
+                    />
+                  </div>
+                </div>
+
+                <div className="w-full md:w-1/2 py-5 px-5 md:px-0 md:py-0 md:h-[50vh] relative flex items-center justify-end md:justify-center">
+                  <div 
+                    style={{
+                      transform: `translate(-${movementX}rem, ${movementY-1}rem)`,
+                      transition: "transform 0.2s",
+                    }}
+                    className="rounded-full bg-transparent w-56 md:w-80 aspect-square "
+                  >
+                    <Image
+                      src={bubble6}
+                      className="w-full h-fit"
+                      alt="bubble"
+                    />
+                  </div>
+                </div>
+
+                <div className="w-full md:w-1/2 py-5 px-5 md:px-0 md:py-0 md:h-[50vh] relative flex items-center justify-start md:justify-center">
+                  <div 
+                    style={{
+                      transform: `translate(-${movementX}rem, -${movementY}rem)`,
+                      transition: "transform 0.2s",
+                    }}
+                    className="rounded-full bg-transparent w-56 md:w-80 aspect-square "
+                  >
+                    <Image
+                      src={bubble7}
+                      className="w-full h-fit"
+                      alt="bubble"
+                    />
+                  </div>
+                </div>
+
+                <div className="w-full md:w-1/2 py-5 px-5 md:px-0 md:py-0 md:h-[50vh] relative flex items-center justify-end md:justify-center">
+                  <div 
+                    style={{
+                      transform: `translate(-${movementX-2}rem, ${movementY}rem)`,
+                      transition: "transform 0.2s",
+                    }}
+                    className="rounded-full bg-transparent w-56 md:w-80 aspect-square "
+                  >
+                    <Image
+                      src={bubble8}
+                      className="w-full h-fit"
+                      alt="bubble"
+                    />
+                  </div>
+                </div>
+            </div>
+            
+            <div className="w-full px-5 py-20 flex flex-col items-center justify-center gap-6">
+                <p className="text-sm md:text-[1.175rem] font-[TTChocolates] text-white text-opacity-90 text-center max-w-[57rem]">
+                  Availability of 24Hrs Wi-Fi internet access in the campus with 205 mbps speed and Comprehensive e-learning environment provided in the campus, enables all the students and faculty members to be connected which is essential for effective learning.
+                </p>
+                <p className="text-sm md:text-[1.175rem] font-[TTChocolates] text-white text-opacity-90 text-center max-w-[55rem]">
+                  Our Digital library has over 5800+ e-books available round the clock through a FTP server on the intranet. Students can access the material and books anytime from anywhere on the campus.
+                </p>
+            </div>
           </div>
         </div>
         <Footer color={true} />
