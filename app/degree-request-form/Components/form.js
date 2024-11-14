4d66c417263c78d45ab5c957b2298df1b92d73d9
Harsh Kumar rattewal <harshrattewal@gmail.com>
2024-11-14 16:27:20 +0530

    Degree Request Form

--- app/degree-request-form/Components/form.js
+++ app/degree-request-form/Components/form.js
@@ -100,7 +100,6 @@ const DegreeForm = ({ Degreeref, showForm }) => {
         })
         .catch((error) => {
           console.log("FAILED...", error);
-
           toast.error("Message failed to send.");
         });
     }
@@ -129,7 +128,7 @@ const DegreeForm = ({ Degreeref, showForm }) => {
                   value={formData.name}
                   onChange={(e) => handleChange("name", e.target.value)}
                   required
-                  className="w-full px-4 py-2 border rounded-lg text-black"
+                  className="w-full px-4 py-2 border rounded-lg text-black focus:outline-none"
                 />
               </div>
               <div>
@@ -140,7 +139,7 @@ const DegreeForm = ({ Degreeref, showForm }) => {
                   value={formData.fatherName}
                   onChange={(e) => handleChange("fatherName", e.target.value)}
                   required
-                  className="w-full px-4 py-2 border rounded-lg text-black"
+                  className="w-full px-4 py-2 border rounded-lg text-black focus:outline-none"
                 />
               </div>
               <div>
@@ -155,7 +154,7 @@ const DegreeForm = ({ Degreeref, showForm }) => {
                     handleChange("enrollmentNumber", e.target.value)
                   }
                   required
-                  className="w-full px-4 py-2 border rounded-lg text-black"
+                  className="w-full px-4 py-2 border rounded-lg text-black focus:outline-none"
                 />
               </div>
               <div>
@@ -166,7 +165,7 @@ const DegreeForm = ({ Degreeref, showForm }) => {
                   value={formData.collegeID}
                   onChange={(e) => handleChange("collegeID", e.target.value)}
                   required
-                  className="w-full px-4 py-2 border rounded-lg text-black"
+                  className="w-full px-4 py-2 border rounded-lg text-black focus:outline-none"
                 />
               </div>
               <div>
@@ -175,7 +174,7 @@ const DegreeForm = ({ Degreeref, showForm }) => {
                   value={formData.branch}
                   onChange={(e) => handleChange("branch", e.target.value)}
                   required
-                  className="w-full px-4 py-2 border rounded-lg text-black"
+                  className="w-full px-4 py-2 border rounded-lg text-black focus:outline-none"
                 >
                   <option value="" disabled selected>
                     Select
@@ -213,7 +212,7 @@ const DegreeForm = ({ Degreeref, showForm }) => {
                       handleChange("otherBranch", e.target.value)
                     }
                     required={formData.branch === "Other" ? true : false}
-                    className="w-full px-4 py-2 border rounded-lg text-black"
+                    className="w-full px-4 py-2 border rounded-lg text-black focus:outline-none"
                   />
                 </div>
               )}
@@ -227,7 +226,7 @@ const DegreeForm = ({ Degreeref, showForm }) => {
                   value={formData.address}
                   onChange={(e) => handleChange("address", e.target.value)}
                   required
-                  className="w-full px-4 py-2 border rounded-lg text-black"
+                  className="w-full px-4 py-2 border rounded-lg text-black focus:outline-none"
                 />
               </div>
               <div>
@@ -238,18 +237,18 @@ const DegreeForm = ({ Degreeref, showForm }) => {
                   value={formData.pinCode}
                   onChange={(e) => handleChange("pinCode", e.target.value)}
                   required
-                  className="w-full px-4 py-2 border rounded-lg text-black"
+                  className="w-full px-4 py-2 border rounded-lg text-black focus:outline-none"
                 />
               </div>
               <div>
                 <label className="block mb-2">Contact Number</label>
                 <input
-                  type="number"
+                  type="tel"
                   placeholder="Enter your Phone Number"
                   value={formData.mobile}
                   onChange={(e) => handleChange("mobile", e.target.value)}
                   required
-                  className="w-full px-4 py-2 border rounded-lg text-black"
+                  className="w-full px-4 py-2 border rounded-lg text-black focus:outline-none"
                 />
               </div>
               <div>
@@ -260,9 +259,53 @@ const DegreeForm = ({ Degreeref, showForm }) => {
                   value={formData.email}
                   onChange={(e) => handleChange("email", e.target.value)}
                   required
-                  className="w-full px-4 py-2 border rounded-lg text-black"
+                  className="w-full px-4 py-2 border rounded-lg text-black focus:outline-none"
                 />
               </div>
+              <div>
+                <label for="paid" className="block mb-2">
+                  Degree Handling and Posting Charges Paid
+                </label>
+                <div className="flex items-center justify-start gap-6">
+                  <div className="flex items-center mr-4 mb-4">
+                    <input
+                      id="radio1"
+                      type="radio"
+                      name="paid"
+                      className="hidden"
+                      value="yes"
+                      checked={formData.paid === "yes"}
+                      onChange={(e) => handleChange("paid", e.target.value)}
+                    />
+                    <label
+                      for="radio1"
+                      className="flex items-center cursor-pointer"
+                    >
+                      <span className="w-4 h-4 inline-block mr-1 border border-grey"></span>
+                      Yes
+                    </label>
+                  </div>
+                  <div className="flex items-center mr-4 mb-4">
+                    <input
+                      id="radio2"
+                      type="radio"
+                      name="paid"
+                      className="hidden"
+                      value="no"
+                      checked={formData.paid === "no"}
+                      onChange={(e) => handleChange("paid", e.target.value)}
+                    />
+                    <label
+                      for="radio2"
+                      className="flex items-center cursor-pointer"
+                    >
+                      <span className="w-4 h-4 inline-block mr-1 border border-grey"></span>
+                      NO
+                    </label>
+                  </div>
+                </div>
+              </div>
+
               <div>
                 <label className="block mb-2">
                   Handling and Posting Charges Receipt{" "}
@@ -270,8 +313,18 @@ const DegreeForm = ({ Degreeref, showForm }) => {
                 <input
                   type="file"
                   required
-                  value={formData.paymentSlip}
-                  onChange={(e) => handleChange("paymentSlip", e.target.value)}
+                  className="focus:outline-none  py-2 rounded-lg"
+                  onChange={(e) => {
+                    const file = e.target.files[0];
+                    if (file && file.size <= 500 * 1024) {
+                      // 500 KB limit
+                      handleChange("paymentSlip", file);
+                    } else {
+                      // Optionally clear the file input if file is too large
+                      e.target.value = "";
+                      alert("File size should be 500KB or less.");
+                    }
+                  }}
                 />
               </div>
             </div>
