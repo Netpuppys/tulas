22df7b2a7aa62a1a1378cbd838432a0f902c7324
Harsh Kumar rattewal <harshrattewal@gmail.com>
2024-11-29 21:54:53 +0530

    Window pproblem solved

--- app/Component/WhyTulasScroll.js
+++ app/Component/WhyTulasScroll.js
@@ -162,17 +162,19 @@ function WhyTulasScroll({ parentRef }) {
   };
 
   const changePosition = () => {
-    // if (!window) {
-    //   return;
-    // }
-    const compHeight = window.innerHeight * 2.5;
-    if (scrollY > 0 && scrollY < compHeight) {
-      console.log("true");
-      return true;
-    }
+    if (typeof window !== "undefined") {
+      // if (!window) {
+      //   return;
+      // }
+      const compHeight = window.innerHeight * 2.5;
+      if (scrollY > 0 && scrollY < compHeight) {
+        console.log("true");
+        return true;
+      }
 
-    console.log("false");
-    return false;
+      console.log("false");
+      return false;
+    }
   };
 
   return (
