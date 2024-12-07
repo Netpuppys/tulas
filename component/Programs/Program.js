3ab7d1cc3f4b8f1b22b357678620813ba616fea7
Harsh Kumar rattewal <harshrattewal@gmail.com>
2024-12-07 17:12:17 +0530

    content font size

--- component/Programs/Program.js
+++ component/Programs/Program.js
@@ -73,7 +73,7 @@ function Program({ parentRef, images, content }) {
       </div>
 
       <div ref={ref} className="z-[9] bg-transparent h-fit relative pl-4">
-        <h5 className="w-full text-white md:w-[50%] text-justify px-4 mx-auto py-10 md:py-20 text-[16px] md:text-[22px] font-[TTChocolates] font-normal">
+        <h5 className="w-full text-white md:max-w-[1000px] text-justify px-4 mx-auto py-10 md:py-20 text-[clamp(15px,4.5vw,30px)] md:text-[clamp(18px,1.3vw,45px)] font-[TTChocolates] font-normal">
           {content}
         </h5>
       </div>
