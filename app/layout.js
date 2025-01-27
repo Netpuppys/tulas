810df11380e634d02de7647f982d11a9a35565db
shourya mishra <shourya.desk@gmail.com>
2025-01-27 12:43:09 +0530

    added microsoft clarity

--- app/layout.js
+++ app/layout.js
@@ -50,17 +50,18 @@ export default function RootLayout({ children }) {
             `,
           }}
         />
-
-        <Script
+{/* clarity */}
+        <script
           id="clarity"
-          strategy="afterInteractive"
+          type="text/javascript"
+          // strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
-            (function(c,l,a,r,i,t,y){
+            function(c,l,a,r,i,t,y){
               c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
               t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
               y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-          })(window, document, "clarity", "script", "pu9uv598ph");
+            })(window, document, "clarity", "script", "pu9uv598ph"
             `,
           }}
         />
