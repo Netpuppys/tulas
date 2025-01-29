909ee0e6ab7e303c8f4cd49c4c308d0d5533e90e
Harsh Kumar rattewal <harshrattewal@gmail.com>
2025-01-29 12:47:23 +0530

    campus life images

--- data/campusLifeImages.js
+++ data/campusLifeImages.js
@@ -1,39 +1,46 @@
 import placeholder from "../public/CampusLife/campusLife.png";
-
+import infrastructure1 from "../public/Homepage/campusLife/infrastructure1.webp";
+import infrastructure2 from "../public/Homepage/campusLife/infrastructure2.webp";
+import infrastructure3 from "../public/Homepage/campusLife/infrastructure3.webp";
+import infrastructure4 from "../public/Homepage/campusLife/infrastructure4.webp";
+import infrastructure5 from "../public/Homepage/campusLife/infrastructure5.webp";
+import computer1 from "../public/Homepage/campusLife/computer1.webp";
+import computer2 from "../public/Homepage/campusLife/computer2.webp";
+import computer3 from "../public/Homepage/campusLife/computer3.webp";
+import computer4 from "../public/Homepage/campusLife/computer4.webp";
+import computer5 from "../public/Homepage/campusLife/computer5.webp";
+import hostel from "../public/Homepage/campusLife/hostel.webp";
+import hostel2 from "../public/Homepage/campusLife/hostel2.webp";
+import hostel3 from "../public/Homepage/campusLife/hostel3.webp";
+import hostel4 from "../public/Homepage/campusLife/hostel4.webp";
+import hostel5 from "../public/Homepage/campusLife/hostel5.webp";
+import sports1 from "../public/Homepage/campusLife/sports1.webp";
+import sports2 from "../public/Homepage/campusLife/sports2.webp";
+import sports3 from "../public/Homepage/campusLife/sports3.webp";
+import sports4 from "../public/Homepage/campusLife/sports4.webp";
+import sports5 from "../public/Homepage/campusLife/sports5.webp";
 const campusLifeImages = [
   {
     title: "Infrastructure",
     images: [
-      "https://tulas-assets.s3.ap-south-1.amazonaws.com/campus-life/infra1.JPG",
-      "https://tulas-assets.s3.ap-south-1.amazonaws.com/campus-life/infra2.JPG",
-      "https://tulas-assets.s3.ap-south-1.amazonaws.com/campus-life/infra3.JPG",
-      "https://tulas-assets.s3.ap-south-1.amazonaws.com/campus-life/infra4.JPG",
-      "https://tulas-assets.s3.ap-south-1.amazonaws.com/campus-life/infra5.png",
+      infrastructure1,
+      infrastructure2,
+      infrastructure3,
+      infrastructure4,
+      infrastructure5,
     ],
   },
   {
     title: "Computer center",
-    images: [placeholder, placeholder, placeholder, placeholder, placeholder],
+    images: [computer1, computer2, computer3, computer4, computer5],
   },
   {
     title: "HOSTEL LIFE",
-    images: [
-      "https://tulas-assets.s3.ap-south-1.amazonaws.com/campus-life/hostel1.png",
-      "https://tulas-assets.s3.ap-south-1.amazonaws.com/campus-life/hostel2.png",
-      placeholder,
-      placeholder,
-      placeholder,
-    ],
+    images: [hostel, hostel2, hostel3, hostel4, hostel5],
   },
   {
     title: "Sports",
-    images: [
-      "https://tulas-assets.s3.ap-south-1.amazonaws.com/campus-life/sports1.png",
-      "https://tulas-assets.s3.ap-south-1.amazonaws.com/campus-life/sports2.png",
-      "https://tulas-assets.s3.ap-south-1.amazonaws.com/campus-life/sports3.png",
-      "https://tulas-assets.s3.ap-south-1.amazonaws.com/campus-life/sports4.png",
-      "https://tulas-assets.s3.ap-south-1.amazonaws.com/campus-life/sports5.JPG",
-    ],
+    images: [sports1, sports2, sports3, sports4, sports5],
   },
 ];
 
