0e57d5795253b39a2223bfecbd3696fce60aef1e
Harsh Kumar rattewal <harshrattewal@gmail.com>
2025-02-05 14:33:16 +0530

    video changed

--- app/Component/Video.js
+++ app/Component/Video.js
@@ -7,48 +7,6 @@ function Video() {
     <div className="relative w-full">
       <div className="flex items-center w-full h-full justify-center">
         <div className="flex flex-col w-full px-5 md:px-0">
-          {/* <h2 className="text-[30px] md:text-[42px] text-center w-full font-[CarotSlab] text-[#007A83] mb-4">
-            BEST ENGINEERING COLLEGE IN DEHRADUN
-          </h2> */}
-          {/* <p className="leading-tight text-[clamp(15px,4.2vw,30px)] md:text-[clamp(10px,1.1vw,45px)] w-[90%] md:w-[60%] font-[TTChocolates] mx-auto md:text-center text-black py-8 md:py-20">
-            Tula’s Institute isn’t just about books and exams—it’s a launchpad
-            for your entire future. Here, it’s all about shaping you into a
-            well-rounded individual, blending top-tier education with real-world
-            experiences. From leadership workshops and hands-on projects to
-            sports and cultural events, Tula’s ensures that every student is
-            equipped to thrive beyond the classroom. You won’t just graduate
-            with a degree; you’ll leave as a confident, capable individual,
-            ready to make a mark in the world with a unique blend of skills,
-            values, and experiences that set you apart.
-          </p> */}
-          {/* <div className="w-full aspect-[9/16] object-cover md:aspect-[12/4.8] mx-auto mb-4">
-            <Image
-              src={videoTest}
-              alt=""
-              className="w-full h-full object-cover"
-            />
-          </div> */}
-          {/* <ReactPlayer
-              url={
-                "https://assets.tulas.edu.in/Campus_Tour___Tula_s_Institute___Dehradun_safmr2%20(1).mp4"
-              }
-              playing
-              muted
-              className="w-full h-full object-cover"
-              loop
-              width="100%"
-              height="100%"
-              playsinline
-              config={{
-                file: {
-                  attributes: {
-                    autoPlay: true,
-                    muted: true,
-                    playsInline: true,
-                  },
-                },
-              }}
-            /> */}
           <video
             autoPlay
             controls
@@ -63,33 +21,6 @@ function Video() {
             className="w-full h-full md:hidden mx-auto"
             src="https://assets.tulas.edu.in/tulasMobile.mp4"
           />
-
-          {/* <p className="leading-tight text-[clamp(15px,4.2vw,30px)] md:text-[clamp(10px,1.1vw,45px)] w-[90%] md:w-[60%] font-[TTChocolates] mx-auto md:text-center text-black py-8 md:py-20">
-            At our institution, versatility is at the heart of our academic
-            offerings. Whatever be your ambition, we have a path to match! For
-            those passionate about technology, we offer{" "}
-            <strong className="text-[#007A83]">
-              BCA and MCA in Computer Applications.{" "}
-            </strong>
-            Engineering enthusiasts can choose from{" "}
-            <strong className="text-[#007A83]">
-              B.Tech, M.Tech, and Diploma programs{" "}
-            </strong>
-            across various disciplines. If you’re business-inclined or have a
-            knack for leadership, our{" "}
-            <strong className="text-[#007A83]">
-              MBA, BBA, and B.Com (Hons) programs{" "}
-            </strong>{" "}
-            are designed for you. Aspiring agriculturalists can explore
-            opportunities with{" "}
-            <strong className="text-[#007A83]">
-              B.Sc (Hons) Agriculture,{" "}
-            </strong>
-            while creative minds can pursue their calling through our{" "}
-            <strong className="text-[#007A83]">
-              Mass Communication course (BAJMC).
-            </strong>
-          </p> */}
         </div>
       </div>
     </div>
