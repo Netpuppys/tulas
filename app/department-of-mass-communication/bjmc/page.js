5cdeb5f3d9a3ceffc643193f3d924131e9f0a79d
Harsh Kumar rattewal <harshrattewal@gmail.com>
2025-02-13 13:05:54 +0530

    bajmc

---
+++ app/department-of-mass-communication/bjmc/page.js
@@ -0,0 +1,113 @@
+"use client";
+import React, { useEffect, useRef, useState } from "react";
+import ScrollImage from "../../../public/Homepage/BannerHome/scrollWidgetGray.png";
+import { IoIosArrowRoundUp } from "react-icons/io";
+import Image from "next/image";
+import CampusLife from "@/app/Component/CampusLife";
+import NavbarLanding from "@/app/graduate-school-of-business/Component/NavbarLanding";
+import BannerLandingPage from "@/app/graduate-school-of-business/Component/BannerLandingPage";
+import FormLandingPage from "@/app/graduate-school-of-business/Component/FormLandingPage";
+import CarouselContent from "@/app/graduate-school-of-business/Component/CarouselContent";
+import CuttingEdge from "@/app/graduate-school-of-business/Component/CuttingEdge";
+import WhyChoose from "@/app/graduate-school-of-business/Component/WhyChoose";
+import FacultyDataLandingPage from "@/app/graduate-school-of-business/Component/FacultyDataLandingPage";
+import Accreditations from "@/app/graduate-school-of-business/Component/Accreditations";
+import Placement from "@/app/graduate-school-of-business/Component/Placement";
+import { facultyData } from "@/app/courses/bjmc/data/data";
+const page = () => {
+  const handleScrollArrow = () => {
+    window.scrollTo({
+      top: 0, // Scroll to the top of the page
+      behavior: "smooth", // Smooth scrolling effect
+    });
+  };
+  const scrollRef = useRef(null);
+  const [scrolled, setScrolled] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      const threshold = 0.3;
+      const sixtyVH = window.innerHeight * threshold;
+
+      if (window.scrollY > sixtyVH) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    // Cleanup the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <>
+      <head>
+        <title>Tula's Institute is best to pursue a career in MBA</title>
+        <meta
+          name="description"
+          content="Join Tula's Institute. Excel in MBA and more with industry-focused programs and dedicated faculty. Become a future leader today"
+        />
+      </head>
+      <body>
+        <div className="w-full min-h-screen h-full relative">
+          {/* Navbar */}
+          <NavbarLanding handleScrollArrow={handleScrollArrow} />
+          {/* <NavbarLandingPage /> */}
+          {/* banner */}
+          <BannerLandingPage />
+          {/* Form */}
+          <div ref={scrollRef}>
+            <FormLandingPage course={5} />
+          </div>
+          {/* Content One */}
+          <CarouselContent />
+          {/* Cutting Edge */}
+          <CuttingEdge scrollRef={scrollRef} heading={"Tula’s BA(Hons.)JMC"} />
+          {/* Why Choose Tulas */}
+          <WhyChoose />
+          {/* At a glance */}
+          {/* <AtGlance /> */}
+          <CampusLife />
+          {/*Faculty */}
+          <div className="pt-8 md:pt-[4%] px-4 md:px-6 flex flex-col items-center justify-center w-full">
+            <h8 className="text-[#007A83] px-4 text-center text-[clamp(10px,7.5vw,50px)] leading-tight md:text-[clamp(10px,3.2vw,50px)] font-[GoudyCatalogue] font-semibold">
+              Our Professors
+            </h8>
+            <h4 className="px-8 w-full my-4 md:my-8 text-center font-[Helvetica] leading-tight text-[clamp(10px,4.2vw,30px)] md:text-[clamp(10px,1.1vw,45px)] text-black">
+              With every lesson, professors are shaping minds and paving the way
+              for future innovations.
+            </h4>
+          </div>
+          <div className="pb-8 md:pb-[4%] px-4 w-full">
+            <FacultyDataLandingPage facultyData={facultyData} />
+          </div>
+          {/* Accreditations */}
+          <Accreditations />
+          {/* Placement */}
+          <Placement scrollRef={scrollRef} />
+          {scrolled && (
+            <button
+              onClick={handleScrollArrow}
+              className="hidden md:block bottom-10 left-10 fixed z-50 w-[100px] aspect-square"
+            >
+              <div className="relative w-full h-full flex justify-center items-center">
+                <Image
+                  className="animate-scrollSpin absolute w-full h-fit"
+                  src={ScrollImage}
+                  alt=""
+                />
+                <IoIosArrowRoundUp className="absolute text-[40px] font-thin text-[#292929]" />
+              </div>
+            </button>
+          )}
+        </div>
+      </body>
+    </>
+  );
+};
+
+export default page;
