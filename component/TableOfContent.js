3d5509fbda485a5de01ea3d9c4150f176da1aa63
Harsh Kumar rattewal <harshrattewal@gmail.com>
2025-02-21 15:27:42 +0530

    underline

--- component/TableOfContent.js
+++ component/TableOfContent.js
@@ -48,7 +48,7 @@ function TableOfContent({ slug }) {
       {tocItems.length > 0 && (
         <div
           className="content-div-blog-table"
-          style={{ fontFamily: "TT Chocolates" }}
+          style={{ fontFamily: "TTChocolates" }}
         >
           <h8 className="text-[28px] md:text-[45px] font-[600]">
             Table of Content
@@ -60,11 +60,11 @@ function TableOfContent({ slug }) {
                 className={`cursor-pointer 
                  ${
                    item.tagName === "h2"
-                     ? "text-[24px] md:text-[32px] underline ml-5 md:ml-10"
+                     ? "text-[24px] md:text-[32px] ml-5 md:ml-10"
                      : item.tagName === "h3"
-                     ? "text-[18px] font-semibold list-disc underline mt-4 text-[#353535] md:text-[24px] ml-10 md:ml-20"
+                     ? "text-[18px] font-semibold list-disc mt-4 text-[#353535] md:text-[24px] ml-10 md:ml-20"
                      : item.tagName === "h4"
-                     ? "text-[16px] text-[#b90124] underline md:text-[20px] ml-14 md:ml-24"
+                     ? "text-[16px] text-[#b90124] md:text-[20px] ml-14 md:ml-24"
                      : ""
                  }`}
               >
