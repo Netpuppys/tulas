09e55b63f38cb1531580a61ca10ff7c5353678d2
netpuppys <digital@netpuppys.com>
2026-01-12 16:48:28 +0530

    clarity

--- app/layout.js
+++ app/layout.js
@@ -50,21 +50,6 @@ export default function RootLayout({ children }) {
             `,
           }}
         />
-        {/* clarity */}
-        <script
-          id="clarity"
-          type="text/javascript"
-          // strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-            function(c,l,a,r,i,t,y){
-              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "pu9ur843jb"
-            `,
-          }}
-        />
         
         {/* Meta Pixel */}
         <Script
@@ -216,6 +201,20 @@ export default function RootLayout({ children }) {
         <MobileProvider>
           <UtmProvider>{children}</UtmProvider>
         </MobileProvider>
+
+          <Script
+          id="microsoft-clarity"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "pu9ur843jb");
+            `,
+          }}
+        />
       </body>
     </html>
   );
